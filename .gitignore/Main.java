7def8af4d3142537710b34932107493b95a5b8d6
João Ricardo da Costa Coelho <ricardocostacoelho@gmail.com>
2017-07-13 19:52:19 +0100

    Update and rename .gitignore to Main.java

---
+++ .gitignore/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jogocavalo;
+import jogocavalo.ui.*;
+
+/**
+ *
+ * @author 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Tabuleiro().setVisible(true);
+    }
+
+}
